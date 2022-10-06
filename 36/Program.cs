d6607d0df58f8e28a7fb0839ec010383fc05f7e3
Maxim <max_isupov@mail.ru>
2022-10-06 17:43:45 +0300

    Решил задачу 36

---
+++ 36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+int[] arr = GetArray(10, -100, 100);
+Console.WriteLine($"[{String.Join(", ", arr)}]");
+Console.Write($"Cуммa элементов, стоящих на нечётных позициях = {SumOdd(arr)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int SumOdd(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+
+    if ( i % 2 != 0)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+ 
+}
